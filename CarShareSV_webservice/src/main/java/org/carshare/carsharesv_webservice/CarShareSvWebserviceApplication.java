e39c4c00143c8a2293ce83d2444ec2c30cd6c2a3
diegocalvo05 <00103322@uca.edu.sv>
2025-06-09 17:52:52 -0600

    init project

---
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/CarShareSvWebserviceApplication.java
@@ -0,0 +1,13 @@
+package org.carshare.carsharesv_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShareSvWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarShareSvWebserviceApplication.class, args);
+    }
+
+}
---
+++ CarShareSV_webservice/src/test/java/org/carshare/carsharesv_webservice/CarShareSvWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carshare.carsharesv_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShareSvWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c71467fed85b35f8ce1aecb5b313fc9fad73f6a5
diegohdez33 <00007222@uca.edu.sv>
2025-06-16 12:31:17 -0600

    HF: Correct Params on Car Repository

--- CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/repository/iCarRepository.java
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/repository/iCarRepository.java
@@ -1,8 +1,6 @@
 package org.carshare.carsharesv_webservice.repository;
 
-import org.carshare.carsharesv_webservice.domain.entity.Brand;
 import org.carshare.carsharesv_webservice.domain.entity.Car;
-import org.carshare.carsharesv_webservice.domain.entity.Year;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,8 +10,8 @@ import java.util.UUID;
 public interface iCarRepository extends JpaRepository<Car, UUID> {
 
     List<Car> findCarsByUserId(UUID userId);
-    List<Car> findCarsByYear(Integer year);
+    List<Car> findCarsByYear(Integer yearId);
     Optional<Car> findCarById(UUID carId);
-    List<Car> findCarsByBrand(String brand);
-    List<Car> findCarsByModel(String model);
+    List<Car> findCarsByBrand(Integer brandId);
+    List<Car> findCarsByModel(Integer modelId);
 }

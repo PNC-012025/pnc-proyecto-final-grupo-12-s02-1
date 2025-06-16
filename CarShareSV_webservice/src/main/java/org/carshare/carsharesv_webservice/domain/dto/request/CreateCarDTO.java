ac83dea26fb3f0e6289b10964a41539264044ccd
diegohdez33 <00007222@uca.edu.sv>
2025-06-16 11:43:56 -0600

    Add: Create Car DTO

---
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/domain/dto/request/CreateCarDTO.java
@@ -0,0 +1,56 @@
+package org.carshare.carsharesv_webservice.domain.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.carshare.carsharesv_webservice.domain.entity.Brand;
+import org.carshare.carsharesv_webservice.domain.entity.Model;
+import org.carshare.carsharesv_webservice.domain.entity.Year;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCarDTO {
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  @Min(2)
+  @Max(5)
+  private Integer doors;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  @Min(1)
+  @Max(7)
+  private Integer capacity;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  @Positive
+  private Float dailyPrice;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  @Pattern(regexp = "^P[0-9A-F]{3,6}$")
+  private String plateNumber;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  private Year year;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  private Model model;
+
+  @NotEmpty
+  @NotNull
+  @NotBlank
+  private Brand brand;
+
+}

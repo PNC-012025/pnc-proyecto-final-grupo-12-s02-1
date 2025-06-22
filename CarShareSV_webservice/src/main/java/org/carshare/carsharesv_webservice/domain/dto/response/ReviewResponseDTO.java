77e65add73d4692b79d2af8214d275bac9addbc5
diegohdez33 <00007222@uca.edu.sv>
2025-06-22 17:57:58 -0600

    add: Review Response DTO

---
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/domain/dto/response/ReviewResponseDTO.java
@@ -0,0 +1,17 @@
+package org.carshare.carsharesv_webservice.domain.dto.response;
+
+import java.util.UUID;
+
+public class ReviewResponseDTO {
+
+    private UUID reviewId;
+
+    private String comment;
+
+    private Integer rating;
+
+    private String userName;
+
+    private UUID carId;
+
+}

848a7a536db3e69a76c01627f300dfc1e7592370
diegohdez33 <00007222@uca.edu.sv>
2025-06-22 17:50:39 -0600

    add: Create Review DTO

---
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/domain/dto/request/CreateReviewDto.java
@@ -0,0 +1,24 @@
+package org.carshare.carsharesv_webservice.domain.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReviewDto {
+
+    @NotBlank
+    @NotEmpty
+    @Length(min =10, max = 255, message = "Description must be min 10 and max 255 characters long")
+    private String comment;
+
+    @NotNull
+    @Min(0)
+    @Max(5)
+    private Integer rating;
+
+}

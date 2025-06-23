f3b3645b83910949e2e72c7161ef8b72e545a9b6
diegohdez33 <00007222@uca.edu.sv>
2025-06-22 18:22:23 -0600

    Add: Review Repository Interface

---
+++ CarShareSV_webservice/src/main/java/org/carshare/carsharesv_webservice/repository/iReviewRepository.java
@@ -0,0 +1,16 @@
+package org.carshare.carsharesv_webservice.repository;
+
+import org.carshare.carsharesv_webservice.domain.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface iReviewRepository extends JpaRepository<Review, UUID> {
+
+    Optional<Review> findReviewByReviewId(UUID reviewId);
+    List<Review> findReviewByUserUserId(UUID userId);
+    List<Review> findReviewByCarCarId(UUID carId);
+
+}
